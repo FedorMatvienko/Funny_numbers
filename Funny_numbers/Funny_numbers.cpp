2f819faa15b1914700ab3b5a2da2ad2156a7d559
fmg1991 <fmg1991@yandex.ru>
2020-05-09 14:56:13 +0300

    Добавьте файлы проекта.

---
+++ Funny_numbers/Funny_numbers.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+int main()
+{
+	int start, end, SValue = 0;//start начальное значение, end конечное значение, SValue сохранённое значение
+	int value = 0;//искомое значение для вычисления
+	int sum;//сумма обострённости цифр
+	int quantity = 0;//количество обострённых цифр
+	setlocale(LC_ALL, "Russian");
+	std::cout << "Введи начальное значение: ";
+	std::cin >> start;
+	std::cout << "Введи конечное значение: ";
+	std::cin >> end;
+	for (; start <= end; start++)
+	{
+		sum = 0;
+		SValue = start;
+		while (SValue != 0)
+		{
+			value = SValue % 10;
+			SValue /= 10;
+			sum += pow(value, value);
+		}
+		if (start == sum)
+		{
+			quantity++;
+		}
+	}
+	std::cout << "Количество смешных чисел равно " << quantity;
+	return 0;
+}
